687caf4187c0fe4648fc7c45a89f6721103e981e
Yash Raj Srivastava <yashraj102723@gmail.com>
2026-01-06 14:54:56 +0530

    [yash] add. added cal2

---
+++ 6jan/cal2/server.js
@@ -0,0 +1,7 @@
+const {add,sub,mul}=require('./calculator');
+let sum=add(10,20);
+let difference=sub(30,15);
+let product=mul(5,6);
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
