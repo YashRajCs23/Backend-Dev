192c9cf74f6605b98518d5c0d9cdad78769af67a
Yash Raj Srivastava <yashraj102723@gmail.com>
2026-01-28 23:39:48 +0530

    [yash] add. added express task

--- expressjs/server.js
+++ expressjs/server.js
@@ -1,63 +1,30 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const users = require("./data.js");
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');   
+app.get("/"||"/home",(req,res)=>{
+    res.send("Home Page");
 });
 
-app.get("/user",(req,res) => {
-    res.status(200).send("User Page");
-});
-
-app.get("/userdetail",(req,res) => {
-    let user = {
-        name:"yash",
-        email:"yash@example.com",
+//http://localhost:3000/users/7/profile?tab=settings&lang=en
+app.get("/users/:id/profile",(req,res)=>{
+    const userId=Number(req.params.id);
+    const tab=req.query.tab;
+    const lang=req.query.lang;
+    const user=users.find(u=>u.id===userId);
+    if(!user){
+        return res.status(404).json({
+            message: "User not found"
+        });
     }
-    res.status(200).json(user);
+    res.json({
+        userId:userId,
+        selectedTab:tab,
+        language:lang,
+        userDetails:user
+    });
 });
 
-//3 more routes
-app.get("/about",(req,res) => {
-    res.status(200).send("About Page");
-});
-app.get("/contact",(req,res) => {
-    res.status(200).send("Contact Page");
-});
-app.get("/services",(req,res) => {
-    res.status(200).send("Services Page");
-});
-
-//7 more routes
-app.get("/products",(req,res) => {
-    res.status(200).send("Products Page");
-});
-
-app.get("/blog",(req,res) => {
-    res.status(200).send("Blog Page");
-});
-
-app.get("/faq",(req,res) => {
-    res.status(200).send("FAQ Page");
-}); 
-
-app.get("/terms",(req,res) => {
-    res.status(200).send("Terms and Conditions Page");
-});
-
-app.get("/privacy",(req,res) => {
-    res.status(200).send("Privacy Policy Page");
-});
-
-app.get("/careers",(req,res) => {
-    res.status(200).send("Careers Page");
-});
-
-app.get("/support",(req,res) => {
-    res.status(200).send("Support Page");
-});
-
-app.listen(3000, () => {
-  console.log('Server is running');
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000 → http://localhost:3000");
 });
