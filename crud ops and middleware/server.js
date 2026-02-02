8eb17e2a9a8f3889e9b8692444edcf7399a63fef
Yash Raj Srivastava <yashraj102723@gmail.com>
2026-02-02 09:50:08 +0530

    [yash] add. added express routing

--- crud ops and middleware/server.js
+++ crud ops and middleware/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
 const app=express();
 import { logFun, userValidation, authMiddleware } from './middleware.js';
+import userRouter from './router/userRouter.js';
+import registrationRouter from './router/registrationRoute.js';
+import dashboardRouter from './router/dashboardRoute.js';
+app.use('/dashboard',dashboardRouter); //mounting the dashboardRouter at /dashboard path
+app.use('/api',userRouter); //mounting the userRouter at /api path
+app.use('/register',registrationRouter); //mounting the registrationRouter at /auth path
 import {config} from 'dotenv';
 config();//to load .env file
 app.use(express.json());//middleware to parse json body
@@ -22,92 +28,6 @@ let data=[
     }
 ]
 
-app.get('/',(req,res)=>{
-    res.status(200).json({
-        message:"home route"
-    });
-});
-
-app.get('/user',(req,res)=>{
-    res.status(200).json({
-        message:"all users",
-        user: data
-    });
-});
-
-app.post('/user',userValidation,(req,res)=>{
-    console.log(req.body);
-    const {username,password}=req.body;
-    //validation
-    if(!username || !password){
-        return res.status(400).json({
-            message:"username and password are required"
-        });
-    }
-    if(password.length<6){
-        return res.status(400).json({
-            message:"password must be at least 6 characters long"
-        });
-    }
-    // let newUser={
-    //     id:user.length+1,
-    //     username,
-    //     password
-    // };
-    //using rest spread operator
-    let newUser={
-        id:data.length+1,
-        ...req.body
-    }
-    data.push(newUser);
-    res.status(201).json({
-        message:"user created",
-        user:newUser
-    });
-})
-
-app.get('/profile', authMiddleware, (req, res) => {
-    res.status(200).json({
-        message: "Welcome to admin profile"
-    });
-});
-
-
-app.put('/user/:id',userValidation,(req,res)=>{
-    let id = parseInt(req.params.id);
-    let {username}=req.body;
-    let userIdx=data.findIndex((ele)=>ele.id===id);
-    if(userIdx===-1){
-        return res.status(404).json({
-            message:"user not found"
-        });
-    }
-    let updatedUser={...data[userIdx], ...req.body};
-    data[userIdx] = updatedUser;
-    res.status(200).json({
-        message:"user updated",
-        user:updatedUser
-    });
-});
-
-app.delete('/user/:id',(req,res)=>{
-    const id = parseInt(req.params.id);
-    const userIdx=data.findIndex((ele)=>ele.id===id);
-    if(userIdx===-1){
-        return res.status(404).json({
-            message:"user not found",
-        });
-    }
-    const userDeleted=data[userIdx];
-    //deleting using splice
-    data.splice(userIdx,1);
-    //deleting using filter
-    //data=data.filter((ele)=>ele.id!==id);
-    res.status(200).json({
-        message:"user deleted",
-        user:userDeleted
-    });
-});
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`);
 });
