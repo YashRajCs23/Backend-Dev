8da72473e54136df6f8fcacb64e581e49eef7300
Yash Raj Srivastava <yashraj102723@gmail.com>
2026-02-05 10:09:00 +0530

    [yash] add. added form in ejs

--- ejs/server.js
+++ ejs/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
-
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(express.urlencoded({ extended: true })); // to parse form data
 //static server
 //csr = client side rendering
 //ssr = server side rendering - seo friendly (search engine optimization) fast than csr
@@ -16,18 +15,53 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+// app.get('/user', (req, res) => {
+//     //binding data to ejs template
+//     let userData = { 
+//         name: 'John Doe',
+//         age: 30,
+//     };
+//     res.render('user',{userData});
+// });
+let userData =[ 
+    {id: 1, name: 'John Doe',age: 30},
+    {id: 2, name: 'Jane Smith',age: 25},
+    {id: 3, name: 'Mike Johnson',age: 35},
+];
+
 app.get('/user', (req, res) => {
-    //binding data to ejs template
-    let userData = { 
-        name: 'John Doe',
-        age: 30,
-    };
-    res.render('user',{userData});
+    res.render('user',{userData});    
+});
+
+app.post('/api/user', (req, res) => {
+    const { name, age } = req.body;
+    let newUserData={
+        id: userData.length + 1,
+        name,
+        age,
+    }
+    userData.push(newUserData);
+    res.redirect('/user');
+});
+
+app.get("/api/user/:id", (req, res) => {
+    const userId = parseInt(req.params.id);
+    const useridx = userData.findIndex(u => u.id === userId);
+    if (useridx === -1) {
+        return res.status(404).render('404');
+    }
+
+    userData.splice(useridx, 1);
+    res.redirect('/user'); 
 });
 
 app.get('/list', (req, res) => {
     let arr=["apple","banana","grapes","mango"];
     res.render('list', { arr });
+});
+
+app.use((req, res) => {
+    res.status(404).render('404');
 });
 
 app.listen(3000, () => {
