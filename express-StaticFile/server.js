60d380d93aef32fb01dbe89bc864f39667970da8
Yash Raj Srivastava <yashraj102723@gmail.com>
2026-02-12 09:25:14 +0530

    [yash] add. added pagination logic

--- express-StaticFile/server.js
+++ express-StaticFile/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 const app = express();
 const PORT = 3000;
@@ -9,16 +10,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
-
-//static files
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  const images = [
-    "img1.jpg",
-    "img2.jpg",
-  ];
-  res.render("gallery", { images });
+  const page = parseInt(req.query.page) || 1;
+  const limit = 10;
+
+  const imagesPath = path.join(__dirname, "public", "images");
+  fs.readdir(imagesPath, (err, files) => {
+    if (err) {
+      return res.status(500).send("Error reading images folder");
+    }
+    const imageFiles = files.filter(file =>
+      file.endsWith(".jpg")
+    );
+
+    const totalImages = imageFiles.length;
+    const totalPages = Math.ceil(totalImages / limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = startIndex + limit;
+
+    const images = imageFiles.slice(startIndex, endIndex);
+
+    res.render("gallery", {
+      images,
+      currentPage: page,
+      totalPages
+    });
+  });
 });
 
 app.listen(PORT, () => {
