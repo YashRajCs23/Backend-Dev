2ced52e5cceb62c6894ae41c818e3ae0851a7275
Yash Raj Srivastava <yashraj102723@gmail.com>
2026-02-16 09:33:17 +0530

    [yash] add. added mini project

--- Employee Payroll system/middleware/logger.js
+++ Employee Payroll system/middleware/logger.js
@@ -1,6 +1,13 @@
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
-  next();
+import fs from 'fs';
+
+const logger=(req, res, next) => {
+  const log = `${new Date().toISOString()} - ${req.method} ${req.originalUrl}\n`;
+  fs.appendFile('server.txt', log, (err) => {
+    if(err){
+      console.error('Error writing to log file', err);
+    }
+    next();
+  });
 };
 
 export default logger;
