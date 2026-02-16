b2d0b92e0708f7b2fd986b55389d271ebddeeace
Yash Raj Srivastava <yashraj102723@gmail.com>
2026-02-16 08:48:05 +0530

    [yash] add. added mini project

--- Employee Payroll system/server.js
+++ Employee Payroll system/server.js
@@ -22,5 +22,5 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.error("MongoDB connection error:", err));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}/api/employees`);
 });
